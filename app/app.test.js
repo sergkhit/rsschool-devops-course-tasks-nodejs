cbf831349ae4efb7e683d2dbdf87b6dda477a829
sergkhit <s.khitrovo@gmail.com>
2024-11-28 23:40:41 +0300

    chnge test

--- app/app.test.js
+++ app/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('..index'); 
+const app = require('./index'); 
 
 describe('GET /', () => {
   it('respond with Simple test app', (done) => {
